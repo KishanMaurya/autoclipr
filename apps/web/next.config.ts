f035c9423ab80472dc056865cd83e7ade7e64d73
kishan maurya <kkmaurya.0095@gmail.com>
2026-06-22 19:29:48 +0530

    fix: allow YouTube image domains (yt3.ggpht.com, i.ytimg.com) in Next.js config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "*.supabase.co" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "yt3.ggpht.com" },
+      { protocol: "https", hostname: "yt3.googleusercontent.com" },
+      { protocol: "https", hostname: "i.ytimg.com" },
     ],
   },
 };
